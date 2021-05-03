49003abf21bad61e59dd78276811043245a924d9
Akatocha <66845412+Akatocha@users.noreply.github.com>
2021-05-03 19:58:49 +0300

    fix docs repush

--- docs/_nuxt/static/1620060742/users/7/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnuxt-static\u002F_nuxt\u002Fstatic\u002F1620060742",layout:"default",error:a,serverRendered:true,routePath:"\u002Fusers\u002F7",config:{_app:{basePath:"\u002Fnuxt-static\u002F",assetsPath:"\u002Fnuxt-static\u002F_nuxt\u002F",cdnURL:a}}}}(null));
