ff92377ce68c21116e0eed58c09ccc79ced535d6
Akatocha <66845412+Akatocha@users.noreply.github.com>
2021-05-03 22:24:43 +0300

    fix: :ambulance: url for github fix

---
+++ docs/_nuxt/static/1620069722/users/1/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnuxt-test-dev\u002F_nuxt\u002Fstatic\u002F1620069722",layout:"default",error:a,serverRendered:true,routePath:"\u002Fusers\u002F1",config:{_app:{basePath:"\u002Fnuxt-test-dev\u002F",assetsPath:"\u002Fnuxt-test-dev\u002F_nuxt\u002F",cdnURL:a}}}}(null));
--- nuxt.config.js
+++ nuxt.config.js
@@ -13,7 +13,7 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: "static",
   router: {
-    base: "/nuxt-static"
+    base: "/nuxt-test-dev"
   },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
